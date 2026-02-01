13e263ce33010f773250f113cc6cd6efa94e5018
smallvalthoss <48263101+smallvalthoss@users.noreply.github.com>
2026-01-31 21:45:38 -0700

    bobber

--- Assets/Scripts/Utilities/RandomizeAnimatorTimeScale.cs
+++ Assets/Scripts/Utilities/RandomizeAnimatorTimeScale.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class RandomizeAnimatorTimeScale : MonoBehaviour
 {
+    [SerializeField]
+    private float percentage = 0.01f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         var animator =  GetComponent<Animator>();
         if (animator != null)
         {
-            animator.speed = Random.Range(0.9f, 1.1f);
+            animator.speed = Random.Range(1f - percentage, 1f + percentage);
         }
     }
 }
